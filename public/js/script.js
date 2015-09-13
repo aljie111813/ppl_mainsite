be9c69d4cd819e02a0bffef9ba853a7f5bc51943
kenblizzard <kenneth.bolico@gmail.com>
2015-09-13 21:04:05 +0800

    updated the aboutus page

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,28 @@
 // CUSTOM JS
-jQuery(window).bind('scroll', function () {
-  if (jQuery(window).scrollTop() > 20) {
+$(document).ready(function () {
+    jQuery(window).bind('scroll', function () {
+        if (jQuery(window).scrollTop() > 20) {
 
-    jQuery('.navbar-default').css({
-      '-webkit-transform': 'translate3d(0, 0, 0)',
-      '-moz-transform': 'translate3d(0, 0, 0)',
-      '-o-transform': 'translate3d(0, 0, 0)',
-      'transform': 'translate3d(0, 0, 0)'
+            jQuery('.navbar-default').css({
+                '-webkit-transform': 'translate3d(0, 0, 0)',
+                '-moz-transform': 'translate3d(0, 0, 0)',
+                '-o-transform': 'translate3d(0, 0, 0)',
+                'transform': 'translate3d(0, 0, 0)'
+            });
+
+        } else {
+
+            jQuery('.navbar-default').css({
+                '-webkit-transform': 'translate3d(0, -100%, 0)',
+                '-moz-transform': 'translate3d(0, -100%, 0)',
+                '-o-transform': 'translate3d(0, -100%, 0)',
+                'transform': 'translate3d(0, -100%, 0)'
+            });
+        }
     });
 
-  } else {
 
-    jQuery('.navbar-default').css({
-      '-webkit-transform': 'translate3d(0, -100%, 0)',
-      '-moz-transform': 'translate3d(0, -100%, 0)',
-      '-o-transform': 'translate3d(0, -100%, 0)',
-      'transform': 'translate3d(0, -100%, 0)'
-    });
-  }
+    var $elem = $(".img-holder > img");
+    var maxSize = Math.max($elem.width(), $elem.height());
+    $elem.width(maxSize).height(maxSize);
 });
