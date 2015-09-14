257804c0a2def76acb3699668a5bcac1a851cc60
kenneth <kenneth.bolico@gmail.com>
2015-09-14 17:38:37 +0800

    Combined the updates of Ian and kenneth

--- public/js/script.js
+++ public/js/script.js
@@ -25,4 +25,63 @@ $(document).ready(function () {
     var $elem = $(".img-holder > img");
     var maxSize = Math.max($elem.width(), $elem.height());
     $elem.width(maxSize).height(maxSize);
+
+
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {                
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 900);
+                    return false;
+                }
+            }
+        });
+    });
+
+    //Scroll Top
+
+    var scrollButton = $("#scroll-top");
+    $(window).scroll(function () {
+        $(this).scrollTop() >= 100 ? scrollButton.fadeIn() : scrollButton.fadeOut();
+    });
+    scrollButton.click(function () {
+        $("html,body").animate({ scrollTop: 0 }, 550);
+    });
+
+
+
+
+    
+});
+
+var pContainerHeight = $('.cd-main-header').height();
+
+$(window).scroll(function () {
+
+    var wScroll = $(this).scrollTop();
+
+    if (wScroll <= pContainerHeight) {
+
+        $('#h1').css({
+            'transform': 'translate(0px, ' + wScroll / 0.7 + '%)'
+        });
+
+        $('#img').css({
+            'transform': 'translate(0px, ' + wScroll / 1.5 + '%)'
+        });
+
+        $('#btn').css({
+            'transform': 'translate(0px, ' + wScroll / 20 + '%)'
+        });
+
+        $('#head-img').css({
+            'transform': 'translate(' + wScroll / 20 + '%, 0px)'
+        });
+
+
+    }
 });
